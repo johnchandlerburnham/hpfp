e121557cb421a88f7d8370164fdaf199181f654e
John C. Burnham <john.c.burnham@gmail.com>
2017-10-10 03:58:23 -0400

    chapter 25

---
+++ 25/Bifunctor.hs
@@ -0,0 +1,51 @@
+module BifunctorInstances where
+
+class Bifunctor p where
+  bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+  bimap f g = first f . second g
+
+  first :: (a -> b) -> p a c -> p b c
+  first f = bimap f id
+
+  second :: (b -> c) -> p a b -> p a c
+  second = bimap id
+
+data Deux a b = Deux a b
+
+instance Bifunctor Deux where
+  bimap f g (Deux a b) = Deux (f a) (g b)
+
+data Const a b = Const a
+
+instance Bifunctor Const where
+  bimap f g (Const a) = Const (f a)
+
+data Drei a b c = Drei a b c
+
+instance Bifunctor (Drei a) where
+  bimap f g (Drei a b c) = Drei a (f b) (g c)
+
+data SuperDrei a b c = SuperDrei a b
+
+instance Bifunctor (SuperDrei a) where
+  bimap f g (SuperDrei a b) = SuperDrei a (f b)
+
+data SemiDrei a b c = SemiDrei a
+
+instance Bifunctor (SemiDrei a) where
+  bimap f g (SemiDrei a) = SemiDrei a
+
+data Quadriceps a b c d = Quadzzz a b c d
+
+instance Bifunctor (Quadriceps a b) where
+  bimap f g (Quadzzz a b c d) = Quadzzz a b (f c) (g d)
+
+data Either' a b = Left' a | Right' b
+
+instance Bifunctor Either' where
+  bimap f g (Left' a) = Left' (f a)
+  bimap f g (Right' b) = Right' (g b)
+
+
+
+
---
+++ 25/Twinplicative.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Twinplicative where
+
+import Control.Applicative (liftA2)
+
+newtype Identity a = Identity { runIdentity :: a }
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+newtype Compose f g a = Compose { getCompose :: f (g a) } deriving (Eq, Show)
+
+instance (Functor f, Functor g) => Functor (Compose f g) where
+  fmap f (Compose fga) = Compose $ (fmap . fmap) f fga
+
+instance (Applicative f, Applicative g) => Applicative (Compose f g) where
+  pure :: a -> Compose f g a
+  pure a = Compose $ pure (pure a)
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (<*>) (Compose f) (Compose a) = Compose $ liftA2 (<*>) f a
+  -- (<*>) (Compose f) (Compose a) = Compose $ (fmap (<*>) f) <*> a
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+  foldMap h (Compose fga) = foldMap (foldMap h) fga
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+  traverse h (Compose fga)= Compose <$> traverse (traverse h) fga
