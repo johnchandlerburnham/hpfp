2332ef630459b865fbf8f7d16738a3022ae559b1
John C. Burnham <jcb@johnchandlerburnham.com>
2018-08-09 16:22:39 -0400

    chapter 5 formatting, edits

--- 05/Arith3Broken.hs
+++ 05/Arith3Broken.hs
@@ -3,7 +3,7 @@ module Arith3Broken where
 main :: IO ()
 main = do
   print $ 1 + 2
-  print 10 
+  print 10
   print $ negate (-1)
   print $ 0 + blah
   where blah = negate 1
--- 05/Sing.hs
+++ 05/Sing.hs
@@ -6,6 +6,10 @@ fstString x = x ++ " in the rain"
 sndString :: String -> String
 sndString x = x ++ " over the rainbow"
 
-sing = if (x < y) then fstString x else sndString y where 
+sing = if (x > y) then fstString x else sndString y where
+       x = "Singin"
+       y = "Somewhere"
+
+sing' = if (x > y) then fstString x else sndString y where
        x = "Singin"
        y = "Somewhere"
