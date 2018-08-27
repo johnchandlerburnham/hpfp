3259642b08cac4ced3868b89034384200dcb7e57
John C. Burnham <jcb@johnchandlerburnham.com>
2018-08-26 23:24:56 -0400

    hangman property testing works

--- 14/hangman/test/Spec.hs
+++ 14/hangman/test/Spec.hs
@@ -2,9 +2,11 @@ module Spec where
 
 import Hangman
 import Test.QuickCheck
+import Test.QuickCheck.Monadic
 import Control.Monad (guard)
 import Data.List (nub)
 import Data.Char (isAsciiLower)
+
 
 
 lowerGen :: Gen Char
@@ -19,6 +21,22 @@ puzzleGen = do
   guesses <- wordGen
   return $ Puzzle word guesses
 
+instance Arbitrary Puzzle where
+  arbitrary = puzzleGen
+
+newtype LowerChar = LowerChar Char deriving Show
+
+instance Arbitrary LowerChar where
+  arbitrary = LowerChar <$> lowerGen
+
+puzzleFill :: Puzzle -> LowerChar -> Property
+puzzleFill p (LowerChar c) = monadicIO $ do
+  p' <- run $ handleGuess p c
+  let fillProp = if elem c (word p)
+                 then elem (Just c) (discovered p')
+                 else not $ elem (Just c) (discovered p')
+  assert fillProp
+
 main :: IO ()
 main = do
-  return ()
+  quickCheck puzzleFill
